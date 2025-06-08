32c8dcc725c40dc5bed78a9cf1c152b6f9c784b8
Nijhan <najmashaaban6@gmail.com>
2025-06-08 15:19:10 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1=2
+let num2=31
+let multiply=num1*num2
+let random =Math.floor(Math.random()*100)+1
+let num3=14
+let num4=5
+let mod=num3%num4
+var max = Math.max(1, 3, 5, 10, 7, 20);
